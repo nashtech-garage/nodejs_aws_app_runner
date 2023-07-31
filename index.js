40b1d80b893c4a9106a1062c66595cdc24fa39c2
Minh Tran <minhtran1@nashtechglobal.com>
2023-07-31 17:57:04 +0700

    Update back to number

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello from AWS App Runner');
-  }).listen(APP_PORT);
+  }).listen(3000);
