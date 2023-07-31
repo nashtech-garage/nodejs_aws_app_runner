193a09f03a2e2ef239da04c1f1bb81d0e47cd8e9
Minh Tran <minhtran1@nashtechglobal.com>
2023-07-31 17:40:16 +0700

    Change to env

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const http = require('http');
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello from AWS App Runner');
-  }).listen(3000);
+  }).listen(APP_PORT);
